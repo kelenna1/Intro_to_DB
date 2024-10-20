b472874b0e6e2de67b260e68fc76443edf1af315
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-10-20 22:40:51 +0100

    decription

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Get the full description of the 'books' table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+  AND TABLE_SCHEMA = 'alx_book_store';
